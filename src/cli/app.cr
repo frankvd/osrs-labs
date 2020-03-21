c4295c0b73d9b8a18011fd3ffb62932257690044
Frank van Dongen <frank.van.dongen@magneds.com>
2020-03-21 19:25:56 +0100

    CLI database option

--- src/cli/app.cr
+++ src/cli/app.cr
@@ -1,6 +1,7 @@
 require "clide"
 require "db"
 require "sqlite3"
+require "option_parser"
 require "../osrs-labs"
 require "./commands/*"
 
@@ -9,6 +10,12 @@ include OSRS::Labs::Persistence
 include OSRS::Labs::Collection
 
 db_file = __DIR__ + "/../../var/storage/osrs.db"
+OptionParser.parse! do |opts|
+  opts.on("-d FILE", "--database FILE", "sqlite database file") do |opt|
+    db_file = opt.to_s
+  end
+end
+
 account_repository = SQLAccountRepository.new "sqlite3://" + db_file
 snapshot_repository = SQLSnapshotRepository.new "sqlite3://" + db_file
 snapshot_service = SnapshotService.new(account_repository, snapshot_repository, HiscoreParser.new, HiscoreDownloader.new)
